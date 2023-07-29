fe1f16fa61eb7a179457fc8f3eb2f13c33822761
Matheus Spindula <matheusluiz98@hotmail.com>
2023-07-28 23:44:19 -0300

    Tela Home

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['page.tsx','api.ts','api.tsx']
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 module.exports = nextConfig
