318171674a169d0326e781c5f5f3c74f3d27c0bd
Matheussop <matheussopluiz@outlook.com>
2023-08-20 15:54:35 -0300

    feat: Add next-seo

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import { globalStyles } from '../styles/global'
 import { SessionProvider } from 'next-auth/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../lib/react-query'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
@@ -14,6 +15,14 @@ export default function App({
   return (
     <QueryClientProvider client={queryClient}>
       <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: '',
+            siteName: 'Ignite Call',
+          }}
+        />
         <Component {...pageProps} />
       </SessionProvider>
     </QueryClientProvider>
